5fbd1b556281c0b5635627cafcb9fa1a5e2452ec
Tharindu Lakshitha <tharindu.lakshitha.susl@gmail.com>
2020-02-29 22:51:04 +0530

    added foreign key attributes for models

--- models/cart.js
+++ models/cart.js
@@ -4,12 +4,12 @@ module.exports = (sequelize, DataTypes) => {
   const Product = sequelize.define('Product');
 
   const Cart = sequelize.define('Cart', {
-    //productId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER,
     quantity: DataTypes.INTEGER,
     userId: DataTypes.INTEGER
   }, {});
   Cart.associate = function(models) {
-    Cart.belongsTo(Product);
+    Cart.belongsTo(Product, { targetKey: "id", foreignKey: "productId" });
   };
   return Cart;
 };
--- models/productsorder.js
+++ models/productsorder.js
@@ -5,13 +5,13 @@ module.exports = (sequelize, DataTypes) => {
   const Product = sequelize.define('Product');
 
   const ProductsOrder = sequelize.define('ProductsOrder', {
-    //productId: DataTypes.INTEGER,
-    //orderId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER,
+    orderId: DataTypes.INTEGER,
     quantity: DataTypes.INTEGER,
   }, {});
   ProductsOrder.associate = function(models) {
-    ProductsOrder.belongsTo(Order);
-    ProductsOrder.belongsTo(Product);
+    ProductsOrder.belongsTo(Order, { targetKey: "id", foreignKey: "orderId" });
+    ProductsOrder.belongsTo(Product, { targetKey: "id", foreignKey: "productId" });
   };
   return ProductsOrder;
 };
